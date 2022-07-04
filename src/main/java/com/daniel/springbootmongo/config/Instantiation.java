962bdc7617b4ba92d11e58490753dae5b42a16db
Daniel <danielmrg@gmail.com>
2022-07-03 21:27:25 -0300

    Referencing User Posts

--- src/main/java/com/daniel/springbootmongo/config/Instantiation.java
+++ src/main/java/com/daniel/springbootmongo/config/Instantiation.java
@@ -41,6 +41,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(daniel));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		daniel.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(daniel);
 	}
 	
 }
