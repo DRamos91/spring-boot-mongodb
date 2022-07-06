1e990cb98b6e0a8e1ebffc6aef1213ee4faf8464
Daniel <danielmrg@gmail.com>
2022-07-05 21:37:30 -0300

    Query with multiple criteria

--- src/main/java/com/daniel/springbootmongo/resources/PostResource.java
+++ src/main/java/com/daniel/springbootmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.daniel.springbootmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,16 @@ public class PostResource {
 	private ResponseEntity<List<Post>> findByTitle(@RequestParam (value="text", defaultValue = "")String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	private ResponseEntity<List<Post>> fullSearch(@RequestParam (value="text", defaultValue = "")String text, 
+			@RequestParam (value="minDate", defaultValue = "")String minDate,
+			@RequestParam (value="maxDate", defaultValue = "")String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.converteDate(minDate, new Date(0L));
+		Date max = URL.converteDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
